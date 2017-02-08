708dd1ecde8f10f27fc28b3eb19f0c7e70520e89
Sergii Danylin <sergii_danylin@epam.com>
2017-02-08 15:37:51 +0200

    lab 3

--- main.go
+++ main.go
@@ -1,31 +1,61 @@
+// package main contains an example on how to use the ReadForm, but with the same way you can do the ReadJSON & ReadJSON
+// you may see on other frameworks binders and all that, Iris has simplicity on it's mind, so I think this way is more simple than others.
 package main
 
 import (
 	"fmt"
-	_ "github.com/lib/pq"
-	"database/sql"
+
+	"github.com/flosch/pongo2"
+
+	"github.com/kataras/iris"
 )
 
-
-const (
-	DB_USER     = "postgres"
-	DB_PASSWORD = "pass_1"
-	DB_NAME     = "postgres"
-)
-
+type Visitor struct {
+	Username string
+	Mail     string
+	Data     []string `form:"mydata"`
+}
 
 func main() {
-	dbconnect := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
-	db, err := sql.Open("postgres", dbconnect)
-	checkErr(err)
-	defer db.Close()
 
+	iris.Get("/", func(ctx *iris.Context) {
+		ctx.SetContentType("text/html ;charset=UTF-8")
+		formTemplate.ExecuteWriter(nil, ctx.Response.BodyWriter())
+	})
 
+	iris.Post("/form_action", func(ctx *iris.Context) {
+		visitor := &Visitor{}
+		err := ctx.ReadForm(visitor)
+		if err != nil {
+			fmt.Println("Error when reading form: " + err.Error())
+		}
+		fmt.Printf("\n Visitor: %v", visitor.Data)
+	})
+
+	fmt.Println("Server is running at :8080")
+	iris.Listen(":8080")
 }
 
+var formTemplate, err = pongo2.FromString(`
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<form action="/form_action" method="post">
+<input type="text" name="Username" />
+<br/>
+<input type="text" name="Mail" /><br/>
+<select multiple="multiple" name="mydata">
+<option value='one'>One</option>
+<option value='two'>Two</option>
+<option value='three'>Three</option>
+<option value='four'>Four</option>
+</select>
+<hr/>
+<input type="submit" value="Send data" />
 
-func checkErr(err error) {
-	if err != nil {
-		panic(err.Error())
-	}
-}
+</form>
+</body>
+</html>
+`)
--- test3.go
+++ test3.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-	"net/http"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/standard"
+	mini "github.com/sasbury/mini"
+	"fmt"
+	"log"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Run(standard.New(":1323"))
+
+	config, err := mini.LoadConfiguration("./cwr.ini")
+	if err != nil {
+		log.Fatal(err)
+	}
+	first := config.String("first", "")
+	fmt.Println("first:",first )
+	second := config.String("second", "")
+	fmt.Println("second:", second)
+	third := config.String("third", "")
+	fmt.Println("third:",third )
+	fourth := config.String("forth", "")
+	fmt.Println("fourth:", fourth)
+	fifth := config.Integer("fifth", 0)
+	fmt.Println("fifth:",fifth )
+	sixt := config.Float("sixt", 0)
+	fmt.Println("sixt:", sixt)
+	seventh := config.Boolean("seventh", true)
+	fmt.Println("seventh:",seventh )
+	eighth := config.Boolean("eighth", true)
+	fmt.Println("eighth:",eighth )
+
 }
